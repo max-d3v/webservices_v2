e641f1df9a0f67e7d0193a7d1c91b04514a9875c
max-d3v <max.maul.desenvolvedor@gmail.com>
2024-09-13 11:13:41 -0300

    added basic clustering

---
+++ src/primary-cluster.ts
@@ -0,0 +1,28 @@
+import cluster from "cluster";
+import os from "os";
+
+const CORE_COUNT = os.cpus().length;
+let USABLE_CORES = CORE_COUNT - 2;
+if (USABLE_CORES < 1) {
+    USABLE_CORES = 1;
+}
+
+console.log("Number of cores used: ", USABLE_CORES);
+
+try {
+    cluster.setupPrimary({
+        exec: './index.js',
+        args: ['--use', 'https']
+    });
+    
+    for (let i = 0; i < USABLE_CORES; i++) {
+        cluster.fork();
+    }
+    
+    cluster.on("exit", (worker, code, signal) => {
+        console.log(`Worker ${worker.id} died`);
+        cluster.fork();
+    });
+} catch(err: any) {
+    console.log("Error on primary cluster: ", err.message);
+}
