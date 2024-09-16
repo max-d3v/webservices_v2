545a8f5b7fc94df58f620487b53bd77dd2fc59f2
max-d3v <max.maul.desenvolvedor@gmail.com>
2024-09-16 11:07:55 -0300

    logs

--- src/main.ts
+++ src/main.ts
@@ -5,19 +5,24 @@ import dotenv from "dotenv";
 const configureEnvironment = () => {
     dotenv.config();
     const mode = process.env.NODE_ENV;
-    console.log("Environment: ", mode);
+
     const envFile = `.env.${mode}`;
     dotenv.config({ path: envFile });
+    console.log("Configurado o ambiente: ", mode);
 }
 
 const startServer = async () => {
     configureEnvironment();
 
+
     const { default: router } = await import("./router/routes");
 
     const routes = router;
+    
+    console.log("Variaveis de ambiente:", process.env);
     const server = new Server(routes);
-    server.start();
+
+    server.start(); 
 }
 
 
