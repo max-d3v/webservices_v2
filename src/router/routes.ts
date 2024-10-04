22b67e16166a408ba9c1676e3dc572ede848065d
max-d3v <max.maul.desenvolvedor@gmail.com>
2024-10-04 15:14:49 -0300

    router and postman organization

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response, Router } from "express";
-import runService from "../Controllers/webservicesController";
-import { SapController } from "../Controllers/sapController";
+import runService from "../Handlers/ServicesHandler";
+import { SapController } from "../Controllers/SapController";
 import { SapServices } from "../services/sap-services";
 
 class Routes {
@@ -19,34 +19,45 @@ class Routes {
     }
 
     private initializeRoutes() {
-        this.router.get("/AtualizarCadastroTodosFornecedores", (req:  Request, res: Response, next: NextFunction) => 
+
+
+        //SUPPLIER ROUTES
+        this.router.patch("/SupplierData/all", (req:  Request, res: Response, next: NextFunction) => 
             this.serviceRunner(() => this.sapControllerInstance.AtualizaCadastroFornecedores('1989-01-01'), req, res, next)
         );
-        
-        this.router.get("/AtualizarCadastroFornecedores", (req:  Request, res: Response, next: NextFunction) => {
+        this.router.patch("/SupplierData/today", (req:  Request, res: Response, next: NextFunction) => {
             const todayIsoString = new Date().toISOString().split('T')[0];
             this.serviceRunner(() => this.sapControllerInstance.AtualizaCadastroFornecedores(todayIsoString), req, res, next);
         });
 
+
+
+        //FISCAL DATA ROUTES
+        this.router.get("/FiscalData/:entityType", (req:  Request, res: Response, next: NextFunction) => {
+            this.serviceRunner(() => this.sapControllerInstance.updateClientsRegistrationData(req.params.entityType), req, res, next)
+        })
+
+        //MISC ROUTES
         this.router.get("/DesativarTodosTicketsVendedor/:userId", (req:  Request, res: Response, next: NextFunction) => {
             this.serviceRunner(() => this.sapControllerInstance.deactiveAllTicketsFromVendor(req.params.userId), req, res, next);
         })
-        
         this.router.get("/BuscarCnpjTodosClientes", (req:  Request, res: Response, next: NextFunction) => {
             this.serviceRunner(() => this.sapControllerInstance.getAllClientsCnpjClear(), req, res, next);
         })
-
-        this.router.get("/updateAllClientsRegistrationData", (req:  Request, res: Response, next: NextFunction) => {
-            this.serviceRunner(() => this.sapControllerInstance.updateAllClientsRegistrationData(), req, res, next)
-        })
-
         this.router.get("/BuscarCnpjTodosFornecedores", (req:  Request, res: Response, next: NextFunction) => {
             this.serviceRunner(() => this.sapControllerInstance.getAllFornecedoresCnpjClear(), req, res, next);
         })
-
         this.router.get("/getMysqlSapClients", (req: Request, res: Response, next: NextFunction) => {
             this.serviceRunner(() => this.sapControllerInstance.getMysqlSapClients(), req, res, next);
         })
+
+
+        //COMPANY DATA ROUTES
+        this.router.get("/company/:taxid", (req: Request, res: Response, next: NextFunction) => {
+            this.serviceRunner(() => this.sapControllerInstance.getCompanyByTaxId(req.params.taxid), req, res, next);
+        })
+
+
 
 
     }
