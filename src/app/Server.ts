3f4554b6a243769f6b70d51cd468dad206bcc7b1
max-d3v <max.maul.desenvolvedor@gmail.com>
2024-12-02 08:05:46 -0300

    added cpu profiling

--- src/app/Server.ts
+++ src/app/Server.ts
@@ -1,10 +1,12 @@
 import express, { Application, NextFunction } from "express";
 import { ErrorHandling } from "../utils/errorHandler";
 import { authMiddleware } from "../middlewares/auth";
-import { SapB1ServiceLayer } from "../Clients/SapB1ServiceLayerClient";
+import { SapB1ServiceLayerServices } from "../Services/SapB1ServiceLayerServices";
 import http from "http";
 import { Router } from "./Router";
 import { Request, Response } from "express";
+import { Session } from 'node:inspector/promises';
+import { writeFile } from "node:fs/promises";
 
 export class HttpError extends Error {
   constructor(public statusCode: number, message: string) {
@@ -17,7 +19,7 @@ export class Server {
   private PORT: number;
   private server: http.Server | null;
   public static instance: Server;
-  
+
   constructor() {
     this.server = null;
     this.app = express();
@@ -26,13 +28,36 @@ export class Server {
 
   public static getInstance(): Server {
     if (!Server.instance) {
-        Server.instance = new Server();
+      Server.instance = new Server();
     }
     return Server.instance;
-}
+  }
+
+  private cpuProfiling() {
+    let _session: Session;
+    return {
+      async start() {
+        _session = new Session();
+        _session.connect();
+
+        await _session.post("Profiler.enable");
+        await _session.post("Profiler.start");
+      },
+
+      async stop() {
+        const { profile } = await _session.post("Profiler.stop");
+        _session.disconnect();
+
+        const profileName = `cpu_profiling/profile-${Date.now()}.cpuprofile`;
+        await writeFile(profileName, JSON.stringify(profile));
+        console.log(`Saved cpu profile ${profileName.split("/")[1]}`);
+        return profile;
+      }
+    }
+  }
 
   private async instanciateSapB1Service() {
-    SapB1ServiceLayer.getInstance();
+    SapB1ServiceLayerServices.getInstance();
   }
 
   private applyMiddlewares(): void {
@@ -41,24 +66,40 @@ export class Server {
   }
 
   private async applyRoutes(): Promise<void> {
-    const router = new Router();    
+    const router = new Router();
     this.app.use("/webservices", (req: Request, res: Response, next: NextFunction) => {
       router.handleRoute(req, res, next)
     });
   }
 
+  private profile_cpu_usage() {
+    const { start, stop } = this.cpuProfiling();
+    start();
+
+    const exitSignals = ["SIGINT", "SIGTERM", "SIGUSR2"];
+    exitSignals.forEach(signal => {
+      process.on(signal, async () => {
+        await stop();
+        console.log(`Stopped cpu profile`);
+        process.exit(0);
+      });
+    })
+  }
+
 
   public async start() {
+    this.profile_cpu_usage()
+
     this.applyMiddlewares();
-    
     await this.instanciateSapB1Service();
     await this.applyRoutes();
 
     this.app.use(ErrorHandling);
-    
+
     this.server = this.app.listen(this.PORT, () => {
       console.log(`Server is running on port ${this.PORT} in ${process.env.NODE_ENV} mode`);
     });
+
   }
 
   public async stop() {
